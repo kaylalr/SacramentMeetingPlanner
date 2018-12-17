b3fb162c08376be4b02f1370a5f793478603fb08
Jalyn Zamora Ortiz <jalynortiz@yahoo.com>
2018-12-17 14:49:59 -0700

    added CRUD to speakers

---
+++ SacramentMeetingPlanner/Pages/Speakers/Edit.cshtml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SacramentMeetingPlanner.Models;
+
+namespace SacramentMeetingPlanner.Pages.Speakers
+{
+    public class EditModel : PageModel
+    {
+        private readonly SacramentMeetingPlanner.Models.MeetingContext _context;
+
+        public EditModel(SacramentMeetingPlanner.Models.MeetingContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Speaker Speaker { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Speaker = await _context.Speakers.FirstOrDefaultAsync(m => m.SpeakerId == id);
+
+            if (Speaker == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Speaker).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SpeakerExists(Speaker.SpeakerId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool SpeakerExists(int id)
+        {
+            return _context.Speakers.Any(e => e.SpeakerId == id);
+        }
+    }
+}
